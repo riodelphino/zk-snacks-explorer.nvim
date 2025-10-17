916fd89edde549ba089f60fe0e9f162ba81abf2c
riodelphino <ryo.heavenlysky@gmail.com>
2025-10-18 03:43:27 +0900

    wip: I'm lost...

--- lua/snacks/zk/finder.lua
+++ lua/snacks/zk/finder.lua
@@ -22,8 +22,6 @@ function Tree:get_zk(cwd, cb, opts)
       table.insert(nodes, n)
    end)
 
-   print("nodes: " .. vim.inspect(nodes)) -- DEBUG: うーんよくわからん。
-
    table.sort(nodes, function(a, b)
       -- 0. ルート優先
       if a.dir and a.path == cwd then
@@ -290,6 +288,50 @@ end
 --    end
 -- end
 
+-- WORKS !!
+-- ---@param opts snacks.picker.explorer.Config
+-- ---@type snacks.picker.finder
+-- local finder = function(opts, ctx)
+--    local state = explorer.get_state(ctx.picker)
+--
+--    if state:setup(ctx) then
+--       return explorer.search(opts, ctx)
+--    end
+--
+--    if opts.git_status then
+--       require("snacks.explorer.git").update(ctx.filter.cwd, {
+--          untracked = opts.git_untracked,
+--          on_update = function()
+--             if ctx.picker.closed then
+--                return
+--             end
+--             ctx.picker.list:set_target()
+--             ctx.picker:find()
+--          end,
+--       })
+--    end
+--
+--    if opts.diagnostics then
+--       require("snacks.explorer.diagnostics").update(ctx.filter.cwd)
+--    end
+--
+--    return function(cb)
+--       local cwd = ctx.filter.cwd
+--
+--       -- get_zk で全 md ノートを配列として取得
+--       Tree:get_zk(cwd, function(n)
+--          -- cb 内で直接渡すので何もしなくてOK
+--          cb(n)
+--       end, {
+--          hidden = opts.hidden,
+--          ignored = opts.ignored,
+--          exclude = opts.exclude,
+--          include = opts.include,
+--          expand = false, -- 必要に応じて変更
+--       })
+--    end
+-- end
+
 ---@param opts snacks.picker.explorer.Config
 ---@type snacks.picker.finder
 local finder = function(opts, ctx)
@@ -318,17 +360,58 @@ local finder = function(opts, ctx)
 
    return function(cb)
       local cwd = ctx.filter.cwd
+      local items = {} ---@type table<string, snacks.picker.explorer.Item>
+      local top = Tree:find(cwd)
+      local last = {} ---@type table<snacks.picker.explorer.Node, snacks.picker.explorer.Item>
 
-      -- get_zk で全 md ノートを配列として取得
-      Tree:get_zk(cwd, function(n)
-         -- cb 内で直接渡すので何もしなくてOK
-         cb(n)
+      -- get_zk で全 md ノートを取得
+      Tree:get_zk(cwd, function(node)
+         print(vim.inspect(node)) -- DEBUG: この node が,parent とかの無い explorer 側？のテーブルになってる！
+         local parent = node.parent and items[node.parent.file] or nil
+         local status = node.status
+         if not status and parent and parent.dir_status then
+            status = parent.dir_status
+         end
+
+         local zk_note = zk.notes_cache[node.path]
+         local title = zk_note and zk_note.title
+
+         local item = {
+            file = node.path,
+            dir = node.dir,
+            open = node.open,
+            dir_status = node.dir_status or (parent and parent.dir_status),
+            text = title or node.path,
+            parent = parent,
+            hidden = node.hidden,
+            ignored = node.ignored,
+            status = (not node.dir or not node.open or opts.git_status_open) and status or nil,
+            last = true,
+            type = node.type,
+            severity = (not node.dir or not node.open or opts.diagnostics_open) and node.severity or nil,
+            title = title,
+         }
+
+         -- 親の last 更新
+         if last[node.parent] then
+            last[node.parent].last = false
+         end
+         last[node.parent] = item
+
+         -- ルートノードは非 hidden に
+         if top == node then
+            item.hidden = false
+            item.ignored = false
+         end
+
+         items[node.path] = item
+         cb(item) -- explorer に流す
       end, {
          hidden = opts.hidden,
          ignored = opts.ignored,
          exclude = opts.exclude,
          include = opts.include,
-         expand = false, -- 必要に応じて変更
+         expand = true, -- フォルダ内の md も取得
       })
    end
 end
