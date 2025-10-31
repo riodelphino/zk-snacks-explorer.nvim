b98ab7d4bdcdd04a04a756461bfe7b68da66b35f
riodelphino <ryo.heavenlysky@gmail.com>
2025-10-31 14:02:14 +0900

    fix: Default sorting in get_sort_key()

--- lua/snacks/zk/util.lua
+++ lua/snacks/zk/util.lua
@@ -16,10 +16,12 @@ local M = {}
 ---@reaturn string
 local function get_sort_key_part(part, entry, is_last)
   local hidden = part:sub(1, 1) == "."
-  local kind = (is_last and not entry.dir) and "#" or "!" -- !:directories -> #:files
-  local visibility = not hidden and "+" or "." -- +:visible -> .:hidden
-  local zk_flag = (is_last and entry.zk and not entry.dir) and "@" or "_" -- @:has zk -> _:none-zk
-  return string.format("[%s%s%s]%s", kind, visibility, zk_flag, part)
+  local kind = (is_last and not entry.dir) and "#" or "!" -- "!":directories -> "#":files
+  local visibility = not hidden and "+" or "." -- "+":visible -> ".":hidden
+  local title = is_last and not entry.dir and entry.zk and (entry.zk.title or entry.zk.metadata and entry.zk.metadata.title)
+  local has_title = (is_last and not entry.dir and entry.zk and title) and "@" or "_" -- "@":has title -> "_":no title
+  local name = is_last and not entry.dir and entry.zk and entry.zk.title or part
+  return string.format("[%s%s%s]%s", kind, visibility, has_title, name)
 end
 
 ---Get a hierarchical sort key based on path
@@ -40,6 +42,7 @@ function M.get_sort_key(entry, root)
   for i, name in ipairs(parts) do
     table.insert(sort_list, get_sort_key_part(name, entry, (i == #parts)))
   end
+  print(table.concat(sort_list))
 
   return table.concat(sort_list)
 end
